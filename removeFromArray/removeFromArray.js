d9aec5928e436e032750f91225cf033c9dc633f8
slowbeeps <gangwarayush921@gmail.com>
2020-08-01 12:34:13 +0530

    completed removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function (...theArgs) {
+    // I need to remove every element from userArray that comes after it and return it.
+    // So i'll make a resultsArray and add every item in userArray to it if it's not in arguments array.
+    // what I'll need: 
+    // loop through each item of the userArray 
+    // a way to check if an item is included in arguments array 
+    // if it's not in arguments array then push it into the resultsArray
+    let resultsArray = [];
+    let userArray = theArgs[0];
 
+    function checkArguments(value) {
+        if (theArgs.includes(value) === false) {
+            resultsArray.push(value);
+        };
+    };
+    userArray.forEach(checkArguments);
+    return resultsArray;
 }
 
 module.exports = removeFromArray
