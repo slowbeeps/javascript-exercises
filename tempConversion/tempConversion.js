1f1e45116a79dadfd095451d1a885a36e865c73d
slowbeeps <gangwarayush921@gmail.com>
2020-08-01 15:55:20 +0530

    completed celsiusfarheniet

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,21 @@
-const ftoc = function() {
-
+const ftoc = function (inF) {
+    // [C] = ([F] − 32) × ​5⁄9
+    let toC = (inF - 32) * 5 / 9;
+    var inC = toC.toFixed(1);
+    return (+inC);
 }
+ftoc(100);
+console.log(typeof(inC))
 
-const ctof = function() {
 
+const ctof = function (inC) {
+    // [F] = [C] × ​9⁄5 + 32	
+    let toF = (inC * 9 / 5) + 32;
+    let inF = toF.toFixed(1);
+    return (+inF);
 }
 
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 }
