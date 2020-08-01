7a2fef31d5cc5b9352d3cf79168913ded4745013
slowbeeps <gangwarayush921@gmail.com>
2020-08-01 10:43:08 +0530

    completed repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string_, numberOfTimes) {
+    if (numberOfTimes < 0) {
+        return ('ERROR')
+    } else {
+        let result = '';
+        for (let i = 1; i <= numberOfTimes; i++) {
+            result += string_;
+        }
+        return (result);
+    }
 }
 
 module.exports = repeatString
